d70673d3626351ae081595573469c55287a9b034
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-29 19:32:08 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  while array do |index|
+    if array[index] == value_to_find
+      return value_to_find
+    else 
+      nil 
+    end 
+  end 
 end
